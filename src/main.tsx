eab0cebf99c6c64f90b3f3b4be9bddd30a2a10ed
kas.goto <meiliunasvilius@gmail.com>
2024-09-07 13:48:18 +0300

    fix for github pages 8

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename='/'>
       <Routes>
-        <Route path='/' /> {<App />}
+        <Route path='/'> {<App />}</Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
