77d3eebadbbd0af66451a321a3359b10649a5147
kas.goto <meiliunasvilius@gmail.com>
2024-09-07 13:46:01 +0300

    fix for github pages 7

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import App from "./App.tsx"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='/' /> {<App />}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
